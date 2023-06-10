ea1c2984b704a1cc451c0de444df3e4d4f09b618
priyanshusingh5099@gmail.com <priyanshusingh5099@gmail.com>
2023-06-10 16:44:15 +0530

    Added post,put and delete method in payrollController

--- EmployeePayrollV3/Controllers/PayrollsController.cs
+++ EmployeePayrollV3/Controllers/PayrollsController.cs
@@ -1,5 +1,7 @@
 ﻿using EmployeePayrollV3.Data;
+using EmployeePayrollV3.DTOs;
 using EmployeePayrollV3.Models.crudModel;
+using EmployeePayrollV3.Models.DBModel;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,12 +13,72 @@ namespace EmployeePayrollV3.Controllers
     {
         ApiDbContext _dbContext = new ApiDbContext();
 
+        //Adding some details for users
+        [HttpPost("[action]")]
+        public IActionResult AddUserDetail(PayrollDTO pays)
+        {
+            var payDetail = _dbContext.Payrolls.FirstOrDefault(p =>  p.Id == pays.payrollId);
+            if(payDetail != null)
+            {
+                return BadRequest("Payroll Already Exists");
+            }
+
+            Salary salary = _dbContext.Salaries.FirstOrDefault(s => s.Id == pays.salaryId);
+            if(salary == null)
+            {
+                return NotFound("Job Detail and Salary Not Available");
+            }
+
+            User user = _dbContext.Users.FirstOrDefault(u => u.Id == pays.userId);
+            if(user == null)
+            {
+                return NotFound("User Not Found");
+            }
+
+            JobClass jobClass = _dbContext.JobClasses.FirstOrDefault(j => j.Id == pays.jobClassId);
+            if (jobClass == null)
+            {
+                return NotFound("No any Job details found for the user");
+            }
+
+            pays.Amt = salary.Amount;
+            pays.TaxCal = salary.Tax;
+            pays.BonusCal = salary.Bonus;
+
+            int NetSalaryCal = 0;
+            pays.Amt = pays.Amt - (int)((pays.TaxCal * pays.Amt) / 100);
+            NetSalaryCal = pays.BonusCal + pays.Amt;
+
+             //pays.EmployeeName = user.FirstName + " " + user.LastName;
+             //pays.JobName = jobClass.JobDescription;
+
+
+            var EmployeePayroll = new Payroll
+            {
+                userId = pays.userId,
+                salaryId = pays.salaryId,
+                jobClassId = pays.jobClassId,
+                month = pays.month,
+                EmployeeName = pays.EmployeeName,
+                JobName = pays.JobName,
+                RoleName = pays.RoleName,
+                NetSalary = NetSalaryCal
+            };
+
+            _dbContext.Payrolls.Add(EmployeePayroll);
+            _dbContext.SaveChanges();
+            return Ok("Details Added Successfully");
+
+        }
+
+
         //Payroll reciepts for admin and employee
-        [HttpGet("GetSlip")]
-        public IActionResult GetPAyroll(int id)
+        [HttpGet("GetSlipById")]
+        public IActionResult GetPayroll(int id)
         {
             var PaySlipById = _dbContext.Payrolls.Where(u => u.Id == id).Select(p => new PayrollDetails
             {
+                payrollId = p.Id,
                 userId = p.userId,
                 jobClassId = p.jobClassId,
                 salaryId = p.salaryId,
@@ -33,6 +95,72 @@ namespace EmployeePayrollV3.Controllers
             }
 
             return Ok(PaySlipById);
+        }
+
+
+        //updating Payroll details
+        [HttpPut("updateDetail")]
+
+        public IActionResult UpdatePayroll(int id, PayrollDTO pays)
+        {
+            
+            Salary salary = _dbContext.Salaries.FirstOrDefault(s => s.Id == pays.salaryId);
+            if (salary == null)
+            {
+                return NotFound("Job Detail and Salary Not Available");
+            }
+
+            User user = _dbContext.Users.FirstOrDefault(u => u.Id == pays.userId);
+            if (user == null)
+            {
+                return NotFound("User Not Found");
+            }
+
+            JobClass jobClass = _dbContext.JobClasses.FirstOrDefault(j => j.Id == pays.jobClassId);
+            if (jobClass == null)
+            {
+                return NotFound("No any Job details found for the user");
+            }
+
+            pays.Amt = salary.Amount;
+            pays.TaxCal = salary.Tax;
+            pays.BonusCal = salary.Bonus;
+
+            int NetSalaryCal = 0;
+            pays.Amt = pays.Amt - (int)((pays.TaxCal * pays.Amt) / 100);
+            NetSalaryCal = pays.BonusCal + pays.Amt;
+
+            var CurrentPayroll = _dbContext.Payrolls.FirstOrDefault(p => p.Id == id);
+            if(CurrentPayroll != null)
+            {
+                CurrentPayroll.userId = pays.userId;
+                CurrentPayroll.jobClassId = pays.jobClassId;
+                CurrentPayroll.salaryId = pays.salaryId;
+                CurrentPayroll.EmployeeName = pays.EmployeeName;
+                CurrentPayroll.JobName = pays.JobName;
+                CurrentPayroll.RoleName = pays.RoleName;
+                CurrentPayroll.NetSalary = NetSalaryCal;
+
+                _dbContext.SaveChanges();
+                return Ok("Details Updated Successfully");
+            }
+
+            return NotFound("Payroll Details Does not Exist");
+            
+        }
+
+        //Deleting Payroll details
+        [HttpDelete("deleteDetails")]
+        public IActionResult DeletePayroll(int id)
+        {
+            var payrollToDelete = _dbContext.Payrolls.Find(id);
+            if(payrollToDelete  != null)
+            {
+                _dbContext.Payrolls.Remove(payrollToDelete);
+                _dbContext.SaveChanges();
+                return Ok("Payroll Details Deleted Successfully");
+            }
+            return NotFound("Payroll details Not Found");
         }
     }
 }
--- EmployeePayrollV3/DTOs/PayrollDTO.cs
+++ EmployeePayrollV3/DTOs/PayrollDTO.cs
@@ -1,15 +1,22 @@
 ﻿using EmployeePayrollV3.Models.DBModel;
 using System.Text.Json.Serialization;
 
-namespace EmployeePayrollV3.Models.crudModel
+namespace EmployeePayrollV3.DTOs
 {
-    public class PayrollDetails
+    public class PayrollDTO
     {
+        [JsonIgnore]
         public int payrollId { get; set; }
         public int userId { get; set; }
         public int jobClassId { get; set; }
         public int salaryId { get; set; }
-        //public DateTime month { get; set; } = DateTime.Now;
+        [JsonIgnore]
+        public int Amt { get; set; }
+        [JsonIgnore]
+        public int TaxCal { get; set; }
+        [JsonIgnore]
+        public int BonusCal { get; set; }
+        public DateTime month { get; set; } = DateTime.Now;
         public string EmployeeName { get; set; }
         public string JobName { get; set; }
         public string RoleName { get; set; }
